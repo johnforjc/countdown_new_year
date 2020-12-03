99ec90ab68a1d49c18bbaea0767607df48698956
unknown <animeloverz072@gmail.com>
2020-12-03 23:29:36 +0700

    final

--- countdown.js
+++ countdown.js
@@ -11,27 +11,8 @@ const MINUTE = 60 * SECOND
 const HOUR = 60 * MINUTE
 const DAY = 24 * HOUR
 
-// Make sure the target date is same with client device's timezone
-const offset = new Date().getTimezoneOffset();
-let timezone = "GMT"
-
-if( offset < 0 )
-{
-    // For GMT +
-    timezone += `+${ offset / -60 }`
-}
-else if ( offset > 0 )
-{
-    // For GMT -
-    timezone += `${ offset / -60 }`
-}
-
-/* 
-    Make target date with client's timezone
-    Change this for any time target
-*/ 
-
-const targetDate = new Date(`1 January 2021 00:00:00 ${timezone}`);
+// Make a target date
+const targetDate = new Date(`1 January 2021 00:00:00`);
 
 function formatDate(value)
 {
